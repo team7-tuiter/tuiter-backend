88585463265997bb1ac74e0e93c6dbf2d1cd2212
ignaciojarruza <ignaciojarruza@gmail.com>
2022-11-16 11:02:44 -0500

    Chat Model + fixed mongoose types

--- mongoose/ChatSchema.ts
+++ mongoose/ChatSchema.ts
@@ -1,20 +1,21 @@
 import mongoose, { Schema } from "mongoose";
-import Message from "../models/Message";
+import MessageType from "../models/Type";
+import Chat from "../models/Chat";
 /**
  * @file A chat schema for chat model.
  */
 
-const ChatSchema = new mongoose.Schema({
+const ChatSchema = new mongoose.Schema<Chat>({
     userId1: {type: Schema.Types.ObjectId, ref: "UserModel"},
     userId2: {type: Schema.Types.ObjectId, ref: "UserModel"},
     messages: [{
-      _id: {type: Schema.Types.String, required: true },
+      id: {type: Schema.Types.String, required: true },
       from: {type: Schema.Types.ObjectId, ref: "UserModel" },
       to: {type: Schema.Types.ObjectId, ref: "UserModel" },
-      type: {type: Schema.Types.String, required: true },
+      type: {type: Schema.Types.String, default: MessageType.String, enum: MessageType },
       message: {type: Schema.Types.String, required: true },
-      dateSent: {type: Schema.Types.Date, required: true }
+      sentOn: {type: Schema.Types.Date, default: Date.now }
     }]
-  }, { collection: "messages" });
+  }, { collection: "chat" });
 
   export default ChatSchema;
