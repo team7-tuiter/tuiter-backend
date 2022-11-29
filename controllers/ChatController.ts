eb6f26192b954b2259c84823e0d36c31c40f6780
ignaciojarruza <ignaciojarruza@gmail.com>
2022-11-29 13:53:47 -0500

    added getSingleChat

--- controllers/ChatController.ts
+++ controllers/ChatController.ts
@@ -10,18 +10,24 @@ export default class ChatController implements ChatControllerI {
     public static getInstance = (app: Express): ChatController => {
         if (ChatController.chatController === null) {
             ChatController.chatController = new ChatController();
-            app.get("users/:uid/chats", ChatController.chatController.getAllChatsById);
-            app.delete("users/uid1/users/uid2/chat", ChatController.chatController.deleteSingleChat);
+            app.get("api/users/:uid/chats", ChatController.chatController.getAllChatsById);
+            app.delete("api/users/:uid1/users/:uid2/chat", ChatController.chatController.deleteSingleChat);
+            app.get("api/users/:uid1/users/:uid2/chat", ChatController.chatController.getSingleChat);
         }
         return ChatController.chatController;
     }
+
     getAllChatsById(req: Request, res: Response): void {
         ChatController.chatDao.getAllChatsById(req.params.uid).then(result => res.json(result));
     }
+
     deleteSingleChat(req: Request, res: Response): void {
         //Need to order uid1 and uid2 if not done on frontend
         ChatController.chatDao.deleteSingleChat(req.params.uid1, req.params.uid2).then(result => res.json(result));
-
+    }
+    
+    getSingleChat(req: Request, res: Response): void {
+        ChatController.chatDao.getSingleChat(req.params.uid1, req.params.uid2).then(result => res.json(result));
     }
     
 }
--- interfaces/ChatControllerI.ts
+++ interfaces/ChatControllerI.ts
@@ -3,4 +3,5 @@ import { Request, Response } from "express";
 export default interface ChatControllerI {
     getAllChatsById(req: Request, res: Response): void;
     deleteSingleChat(req: Request, res: Response): void;
+    getSingleChat(req: Request, res: Response): void;
 }
