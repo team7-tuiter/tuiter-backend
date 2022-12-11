ab4ebadf2f96cc7bba498515e10142d2366cec18
ignaciojarruza <85847657+ignaciojarruza@users.noreply.github.com>
2022-12-11 13:47:57 -0500

    added environment port

--- server.ts
+++ server.ts
@@ -30,6 +30,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(process.env.PORT);
 
 const options = {
   useNewUrlParser: true,
